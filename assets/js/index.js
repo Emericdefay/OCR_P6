772cdde820ad93d1876782951fc32af4fc9e64c2
emericdefay <emericdefay@gmail.com>
2021-04-16 13:05:56 +0200

    Adding modales

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-//import "./modale"
-
 // url API
 const url = "http://localhost:8000/api/v1/titles/";
 
@@ -10,37 +8,48 @@ let cat1 = document.querySelector("#cat1");
 let cat2 = document.querySelector("#cat2");
 let cat3 = document.querySelector("#cat3");
 
-// // attributes
-// const image = document.querySelector("#image");
-// const title = document.querySelector("#title");
-// const genres = document.querySelector("#genres");
-// const released = document.querySelector("#date_published");
-// const rated = document.querySelector("#rated");
-// const imdb = document.querySelector("#imdb_score");
-// const directors = document.querySelector("#directors");
-// const actors = document.querySelector("#actors");
-// const duration = document.querySelector("#duration");
-// const countries = document.querySelector("#countries");
-// const boxOffice = document.querySelector("#worldwide_gross_income");
-// const description = document.querySelector("#description");
-
-
-//showMovies();
 start();
 
-function createMoviePoster(movies)
+function createMoviePoster(movies, parent)
 {
     /*
      *
      */
     return movies.map((movie)=>
         {
-        return `<div class="carousel__slide">\
-        <img src=${movie.image_url} class="poster" movie-id =${movie.id} onclick="newPage(${movie.id})"/>\
-        </div>`;
-
+        return `<div \
+                class="carousel__slide">\
+                    <a href="#${movie.id}" \
+                    data-target="#${movie.id}" \
+                    data-toggle="modal">
+                        <img \
+                        src=${movie.image_url} \
+                        class="poster"/>\
+                    </a>
+                </div>
+                
+                <div \
+                id="${movie.id}" \
+                class="modal">\
+                    <div \
+                    class="modal__content" \
+                    style="background-image: url(${movie.image_url});
+                            background-size:cover;">\
+                        <h1>${movie.title}</h1>\
+                        <p>Genre : ${movie.genre}</p>\
+                        <p>Date released : ${movie.date_published}</p>\
+                        <p>Rate : "${movie.rated}"</p>\
+                        <p>IMDB Score : "${movie.imdb_score}"</p>\
+                        <p>Directors : "${movie.directors}"</p>\
+                        <p>Actors : "${movie.actors}"</p>\
+                        <p>Duration : "${movie.duration}"</p>\
+                        <p>Countries : "${movie.countries}"</p>\
+                        <p>Box office : "${movie.worldwide_gross_income}"</p>\
+                        <p>Description : ${movie.description}</p>\
+                        <a href="#${parent}" class="modal_close">&times;</a>\
+                    </div>\
+                </div>`;
         })                  
-
 }
 
 async function getData(url, start, range)
@@ -51,18 +60,6 @@ async function getData(url, start, range)
             {
                 return createMoviePoster(data.results.slice(start, range));
             });
-
-
-    // return new Promise((resolve, reject) =>
-    // { 
-    //     fetch(url)
-    //         .then((res) => res.json())
-    //         .then((data)=>
-    //         {
-    //             return data
-    //             resolve(createMoviePoster(name, data.results.slice(start, range)));
-    //         });
-    // })
 }
 
 async function getPostersIndex(command)
@@ -73,24 +70,48 @@ async function getPostersIndex(command)
     const data2 = await getData(newUrl, 0, 2);
     const data3 = data.concat(data2);
     return data3;
-    //return element;
 }
-
-//
 
 function createMovieInfo(movie)
 {
     /*
      *
      */
-    console.log(movie)
     return `<div class="informations">\
-            <img src=${movie.image_url} class="bigPoster"/>
-            <p class="title">${movie.title}</p>\
-                <div>\
-                    <p class="desc">${movie.description}</p>\
+                <a href="#${movie.id}" \
+                data-target="#${movie.id}" \
+                data-toggle="modal"> \
+                    <img\
+                    src=${movie.image_url} \
+                    class="bigPoster" \
+                    >\
+                </a> \
+                <p class="title">${movie.title}</p>\
+                    <div>\
+                        <p class="desc">${movie.description}</p>\
+                    </div>\
+            </div>\
+            <div \
+            id="${movie.id}" \
+            class="modal">\
+                <div \
+                class="modal__content" \
+                style="background-image: url(${movie.image_url});
+                        background-size:cover;">\
+                    <h1>${movie.title}</h1>\
+                    <p>Genre : ${movie.genre}</p>\
+                    <p>Date released : ${movie.date_published}</p>\
+                    <p>Rate : "${movie.rated}"</p>\
+                    <p>IMDB Score : "${movie.imdb_score}"</p>\
+                    <p>Directors : "${movie.directors}"</p>\
+                    <p>Actors : "${movie.actors}"</p>\
+                    <p>Duration : "${movie.duration}"</p>\
+                    <p>Countries : "${movie.countries}"</p>\
+                    <p>Box office : "${movie.worldwide_gross_income}"</p>\
+                    <p>Description : ${movie.description}</p>\
+                    <a href="#" class="modal_close">&times;</a>\
                 </div>\
-            </div>`;               
+            </div>`; 
 }
 
 async function getUrlBestMovie(url)
@@ -112,122 +133,10 @@ async function getBestMoviePoster()
             .then((res)=>res.json())
             .then((data)=>
                 {
-                    console.log(data)
                     return createMovieInfo(data)
                 });
 }
 
-// async function getImage(url){
-//     /*
-//      *
-//      */
-//     const movieElement = document.createElement("div");
-//     movieElement.setAttribute("class", "image");
-
-//     fetch(url)
-//         .then((res)=>res.json())
-//         .then((data)=>
-//         {
-//            movieElement.innerHTML = `<img src=${data["image_url"]}/>`;
-//         });
-//     return movieElement;
-// }
-
-// async function getDetails(url, command)
-// {
-//     /*
-//      *
-//      */
-//     const movieElement = document.createElement("div");
-//     movieElement.setAttribute("class", command);
-
-//     fetch(url)
-//         .then((res)=>res.json())
-//         .then((data)=>
-//         {
-//             movieElement.innerHTML = `<p>${command} : ${data[`${command}`]}</p>`;
-//         });
-//     return movieElement;
-// }
-
-// function newPage(id_page)
-// {
-//     /*
-//      *
-//      */
-//     window.open("page.html?"+id_page, "_top");
-// }
-
-// if (location.pathname.includes("/page.html"))
-// {
-//     window.onload = function()
-//     {
-//         /*
-//          *
-//          */
-
-//         let address = document.location + "";
-//         const id = address.split("?").pop();
-//         const newUrl = url + id;
-
-//         // Fetch :
-//         const pageImage = getImage(newUrl);
-//         const pageTitle = getDetails(newUrl, "title");
-//         const pageGenres = getDetails(newUrl, "genres");
-//         const pageReleased = getDetails(newUrl, "date_published");
-//         const pageRated = getDetails(newUrl, "rated");
-//         const pageImdb = getDetails(newUrl, "imdb_score");
-//         const pageDirectors = getDetails(newUrl, "directors");
-//         const pageActors = getDetails(newUrl, "actors");
-//         const pageDuration = getDetails(newUrl, "duration");
-//         const pageCountries = getDetails(newUrl, "countries");
-//         const pageBoxOffice = getDetails(newUrl, "worldwide_gross_income");
-//         const pageDescription = getDetails(newUrl, "description");
-
-//         // AppendChild :
-//         image.appendChild(pageImage);
-//         title.appendChild(pageTitle);
-//         genres.appendChild(pageGenres);
-//         released.appendChild(pageReleased);
-//         rated.appendChild(pageRated);
-//         imdb.appendChild(pageImdb);
-//         directors.appendChild(pageDirectors);
-//         actors.appendChild(pageActors);
-//         duration.appendChild(pageDuration);
-//         countries.appendChild(pageCountries);
-//         boxOffice.appendChild(pageBoxOffice);
-//         description.appendChild(pageDescription);
-//     };
-// }
-
-function showMovies()
-{
-    //
-
-
-
-    // Fetch :
-    let movieCatBestMovies = getPostersIndex("?sort_by=-imdb_score");
-    let movieCat1 = getPostersIndex("?genre=Fantasy");
-    let movieCat2 = getPostersIndex("?genre=Drama");
-    let movieCat3 = getPostersIndex("?genre=Family");
-
-    // Get images :
-         
-    // bestMovies.appendChild(movieCatBestMovies);
-    // cat1.appendChild(movieCat1);
-    // cat2.appendChild(movieCat2);
-    // cat3.appendChild(movieCat3);
-
-    bestMovies=movieCatBestMovies;
-    cat1=movieCat1;
-    cat2=movieCat2;
-    cat3=movieCat3;
-
-    const start = new CustomEvent("start");
-
-    document.dispatchEvent(start);
-}
 
 class SlideShow
 {
@@ -297,18 +206,15 @@ class SlideShow
     nextSlide()
     {
         this.translate(this.currentSlide + this.options.slidesToScroll);
-
     }
 
     prevSlide()
     {
-        this.translate(this.currentSlide - this.options.slidesToScroll);
-        
+        this.translate(this.currentSlide - this.options.slidesToScroll); 
     }
 
     translate(index)
     {
-
         let translation = -0.8*index*this.options.slidesToScroll*(this.container.offsetWidth / this.items.length);
         this.container.style.translation
         this.container.style.transform = "translate3d("+translation+"px, 0px, 0px)";
